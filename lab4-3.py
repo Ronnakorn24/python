af44a94386bc87c9376f4e21a845d614a9c2b9ae
maximus123ads <6629011020@cdti.ac.th>
2024-06-24 16:25:30 +0700

    updata

---
+++ lab4-3.py
@@ -0,0 +1,9 @@
+person = {"name": "Ronnakorn", "age":18, "wt":55, "h":165}
+#for n in person
+#    print(n)
+print(person["name"])
+print(person["age"])
+print(person["wt"])
+print(person["h"])
+person["name"]="Ronnakorn"
+print(person["name"])
